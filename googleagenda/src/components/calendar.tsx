00c9ebae9832feafa96891fa735bf908df5ee008
victor <victorfire99@gmail.com>
2024-04-04 21:23:20 -0300

    codificação

---
+++ googleagenda/src/components/calendar.tsx
@@ -0,0 +1,9 @@
+ export default function Calendar(props:any){
+    return(
+        <div>
+            <div className="conteiner-calendar">
+            <iframe className="calendar" src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FFortaleza&bgcolor=%23ffffff&src=dmljdG9yZmlyZTk5QGdtYWlsLmNvbQ&src=Y2xhc3Nyb29tMTEyOTc4MTA1Njc2OTQwMTQwMDU5QGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20&src=cHQuYnJhemlsaWFuI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&color=%23039BE5&color=%23007b83&color=%230B8043"></iframe>
+            </div>
+        </div>
+    )
+ }
